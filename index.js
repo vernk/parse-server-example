1e50549b7a3de95ef9566b7c1f2463d99cf27a30
vernk <vernk@techie.com>
2016-03-11 21:45:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337'  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337',  // Don't forget to change to https if needed
   clientKey: process.env.CLIENT_KEY || 'noClientKey',
   javascriptKey: process.env.JAVSCRIPT_KEY || 'noJavascriptKey',
   
